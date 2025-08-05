ddec20c9d69dcf2369af359a287d7e527e0c7623
King Ballo <stouvoli@gmail.com>
2025-08-05 16:10:35 +0000

    Test du Gate

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // !!! VULNÉRABILITÉ INTENTIONNELLE POUR LE TEST SAST !!!
 const dbPassword = "SuperSecretPassword123!";
+// On ajoute une fausse clé API directement dans le code pour le test
+const FAKE_API_KEY = "sqp_a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0"; // VULNERABILITY
 const app = express();
 const PORT = 3000;
 
